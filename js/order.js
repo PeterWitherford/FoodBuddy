90f4ffe8511235d6f711b4a0a38056265a14f0b7
Peter Witherford <Peter Witherford>
2017-05-08 10:42:04 +0100

    basket

--- js/order.js
+++ js/order.js
@@ -6,12 +6,14 @@ function MenuItem(id,longName,price){
 
 var basket=[];
 
+
 var ribs = new MenuItem("ribs", "Spare Ribs in Barbecue Sauce", 4.60);
 var ccm = new MenuItem("ccm", "Chicken Chow Mein", 4.20);
 var cfr = new MenuItem("cfr", "Chicken Fried Rice", 4.20);
 var cc = new MenuItem("cc", "Curry Chicken", 4.70);
 var ssc = new MenuItem("ssc", "Sweet and Sour Chicken Crispy Balls", 4.00);
 
+var basketTotal =document.getElementById("basketTotal");
 var ribbutton=document.getElementById("ribs");
 var ccmbutton=document.getElementById("ccm");
 var cfrbutton=document.getElementById("cfr");
@@ -28,4 +30,5 @@ sscbutton.onclick = function(){addToBasket(ssc)}
 function addToBasket(item){
     basket.push(item);
     Materialize.toast(item.longName + " added to basket", 4000);
+    basketTotal.innerHTML("("+basket.length+1+")")   
 }
