93fbd250d01f2520c3e8588fad488276833fb19d
Peter Witherford <Peter Witherford>
2017-05-08 01:42:54 +0100

    cart

---
+++ js/order.js
@@ -0,0 +1,30 @@
+function MenuItem(id,longName,price){
+    this.id = id;
+    this.longName = longName;
+    this.price = price;
+  }
+
+var basket=[];
+
+var ribs = new MenuItem("ribs", "Spare Ribs in Barbecue Sauce", 4.60);
+var ccm = new MenuItem("ccm", "Chicken Chow Mein", 4.20);
+var cfr = new MenuItem("cfr", "Chicken Fried Rice", 4.20);
+var cc = new MenuItem("cc", "Curry Chicken", 4.70);
+var scc = new MenuItem("scc", "Sweet and Sour Chicken Crispy Balls", 4.00);
+
+var ribbutton=document.getElementById("ribs");
+var ccmbutton=document.getElementById("ccm");
+var cfrbutton=document.getElementById("cfr");
+var ccbutton=document.getElementById("cc");
+var sccbutton=document.getElementById("scc");
+
+ribbutton.onclick = function(){addToBasket("ribs")}
+ccmbutton.onclick = function(){addToBasket("ccm")}
+cfrbutton.onclick = function(){addToBasket("cfr")}
+ccbutton.onclick = function(){addToBasket("cc")}
+sccbutton.onclick = function(){addToBasket("scc")}
+
+
+function addToBasket(item){
+    basket.push(item);
+}
